40e1a9f1aba35ef9af4e90ad57c02dd0ef1c51c7
daniel endaylalu <endaylaludaniel@gmail.com>
2023-10-11 16:19:29 +0300

    finished ex 3.7-3.8

--- phonebook/index.js
+++ phonebook/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const morgan = require("morgan");
 
 let persons = [
   {
@@ -22,8 +23,22 @@ let persons = [
     number: "39-23-6423122",
   },
 ];
+
+const unknownEndpoint = (req, res) => {
+  res.status(404).send({ error: "unknown endpoint" });
+};
+
 const app = express();
 app.use(express.json());
+
+morgan.token("body", (req, res) => JSON.stringify(req.body));
+
+// Morgan middleware
+app.use(
+  morgan(
+    ":method :url :status :response-time ms - :res[content-length] :body - :req[content-length]"
+  )
+);
 
 const date = new Date();
 
@@ -40,14 +55,20 @@ app.get("/api/persons/:id", (req, res) => {
   res.json(person);
 });
 app.post("/api/persons", (req, res) => {
-  const id = Math.floor(Math.random() * (max - min + 1)) + min;
-  console.log(id);
+  const id = Math.floor(Math.random() * (1000 - 0 + 1)) + 0;
   const body = req.body;
+  console.log(id, body, body.name);
   if (!body.name && !body.number) {
     return res.status(400).json({
       error: "all fileds required",
     });
-  } else if (persons.find((p) => p.name === body.name)) {
+  }
+  if (
+    persons.find((p) => {
+      console.log(p.name, body.name);
+      return p.name === body.name;
+    })
+  ) {
     return res.status(400).json({
       error: "name must be unique",
     });
@@ -57,6 +78,7 @@ app.post("/api/persons", (req, res) => {
     number: body.number,
     id: id,
   };
+  persons = persons.concat(person);
   res.status(200).json(person);
 });
 app.get("/info", (req, res) => {
@@ -64,6 +86,9 @@ app.get("/info", (req, res) => {
     `<p>Phonebook has info for ${persons.length} people</p><p>${date}</p>`
   );
 });
+
+app.use(unknownEndpoint);
+
 app.listen("3000", (req, res) => {
   console.log("server started on port 3000");
 });
