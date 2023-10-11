4868e608bba5ada2fe255e9c27846d969215b5e6
daniel endaylalu <endaylaludaniel@gmail.com>
2023-10-11 13:55:53 +0300

    added express and create routes

--- phonebook/index.js
+++ phonebook/index.js
@@ -1,12 +1,39 @@
 const express = require("express");
-
 const app = express();
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("<p>hello there!</p>");
-});
+const persons = [
+  {
+    id: 1,
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: 2,
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: 3,
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: 4,
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
+const date = new Date();
 
+app.get("/api/persons", (req, res) => {
+  res.json(persons);
+});
+app.get("/info", (req, res) => {
+  res.send(
+    `<p>Phonebook has info for ${persons.length} people</p><p>${date}</p>`
+  );
+});
 app.listen("3000", (req, res) => {
   console.log("server started on port 3000");
 });
