c108188995ba9a0681fec9d0bfe67c83b698f412
daniel endaylalu <endaylaludaniel@gmail.com>
2023-11-15 19:40:08 +0300

    finished ex7.10-7.17

--- bloglist-frontend/src/pages/Blog.jsx
+++ bloglist-frontend/src/pages/Blog.jsx
@@ -3,50 +3,32 @@ import { useState } from "react";
 import { handleLike } from "../store/blogSlice";
 
 const Blog = ({ blog }) => {
-  const [showDetail, setShowDetail] = useState(false);
   const dispatch = useDispatch();
   if (!blog) {
     return null;
   }
 
   return (
-    <div>
-      <h3 className="card">
-        {blog.title}{" "}
-        <button onClick={() => setShowDetail(!showDetail)}>
-          {showDetail ? "hide" : "View"}
-        </button>
-      </h3>
-      {showDetail && (
-        <div className="card">
-          link:<a href={blog.url}> {blog.url}</a>
-          <p>
-            likes: {blog.likes}{" "}
-            <button
-              type="button"
-              onClick={() => {
-                console.log(blog);
-                dispatch(
-                  handleLike({ ...blog, likes: blog.likes + 1 }, blog.id)
-                );
-              }}
-            >
-              Like
-            </button>
-          </p>
-          <p>author: {blog.author}</p>
-          <p>name: {blog.user.name}</p>
+    <div className="card">
+      <h3>{blog.title} </h3>
+
+      <div>
+        link:<a href={blog.url}> {blog.url}</a>
+        <p>
+          likes: {blog.likes}{" "}
           <button
             type="button"
             onClick={() => {
-              dispatch(handleDelete(blog, blog.id));
-              setShowDetail(false);
+              console.log(blog);
+              dispatch(handleLike({ ...blog, likes: blog.likes + 1 }, blog.id));
             }}
           >
-            Delete
+            Like
           </button>
-        </div>
-      )}
+        </p>
+        <p>author: {blog.author}</p>
+        <p>name: {blog.user.name}</p>
+      </div>
     </div>
   );
 };
